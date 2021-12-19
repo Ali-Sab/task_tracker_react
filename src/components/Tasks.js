b089c3796de8905d1c832853d20e275ed0345743
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2021-12-19 16:41:17 -0500

    Now 60% done. Added stlying for reminders, button to delete tasks added as well

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,12 +1,12 @@
 import Task from "./Task"
 
-const Tasks = ({ tasks, onDelete }) => {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     // Below is how to use state changing function
     //setTasks([...tasks, { id: 3, text: "Marathon", day: "Day after tomorrow", reminder: false}])
     return (
         <>
           {tasks.map((task) => (
-            <Task key={task.id} task={task} onDelete={onDelete} />
+            <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} />
           ))}
         </>
     )
