0d96d82f43d264a84d8c351ef29284e1d019013d
Alison Sabuwala <alison.sabuwala1024@gmail.com>
2021-12-26 00:55:20 -0500

    Project is complete. React router is updated for v6 of react router dom

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 const Header = (props) => {
+    const location = useLocation()
+    
     return (
         <header className='header'>
             <h1 style={headingStyle}>{props.title}</h1>
-            <Button
+            {location.pathname === '/' && <Button
                 color={props.showAddTask ? 'red' : 'green'}
                 text={props.showAddTask ? 'Close' : 'Add Task'}
                 onClick={props.onAdd}
-            />
+            />}
         </header>
     )
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,6 @@
 import Task from "./Task"
 
 const Tasks = ({ tasks, onDelete, onToggle }) => {
-    // Below is how to use state changing function
-    //setTasks([...tasks, { id: 3, text: "Marathon", day: "Day after tomorrow", reminder: false}])
     return (
         <>
           {tasks.map((task) => (
